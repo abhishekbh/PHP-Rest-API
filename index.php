3ca19e7db5d24446729137a4cf805c017cb201fb
Abhishek Bhatnagar <abhishekbh@gmail.com>
2015-01-03 19:46:46 -0800

    delegate before you relegate bro

--- index.php
+++ index.php
@@ -1,5 +1,29 @@
 <?php
 require('bootstrap.php');
+require('api.php');
+
+delegate($_SERVER['REQUEST_METHOD']);
 
 echo "Hooray!";
 
+// FUNCTIONS //
+
+function delegate($requestMethod){
+  $api = new api_class();
+  $params = $_REQUEST;
+
+  switch($requestMethod){
+    case 'GET':
+      $id = $_GET['id'];
+      return $api->get($id);
+    case 'POST':
+      return $api->create($params);
+    case 'PUT':
+      $id = $_PUT['id'];
+      return $api->update($id,$params);
+    case 'DELETE':
+      return $api->delete($_REQUEST);
+    default:
+      error_log('Unknown Request');
+  }
+}

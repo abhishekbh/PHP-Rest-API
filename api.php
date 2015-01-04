9d58fa5a31ccdf4a97184c749132df75af2d1b11
Abhishek Bhatnagar <abhishekbh@gmail.com>
2015-01-03 19:46:12 -0800

    better

--- api.php
+++ api.php
@@ -5,12 +5,25 @@
 * REST API
 * Create -> Post
 * Update -> Put
-* Get -> List
+* Get -> Get
 * Delete -> Delete
 *
 */
+class api_class{
 
-public function create(){}
-public function update(){}
-public function get(){}
-public function delete(){}
+  public function create(array $params){
+    return false;
+  }
+
+  public function get($identifier=null){
+    return false;
+  }
+
+  public function update($identifier,array $params){
+    return false;
+  }
+
+  public function delete($request){
+    return false;
+  }
+}
